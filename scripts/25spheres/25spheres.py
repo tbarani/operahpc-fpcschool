e5b85fba9e8bc92955a23d1e62957a523e7bcc3a
BARANI Tommaso 263902 <tommaso.barani@cea.fr>
2025-12-22 15:28:26 +0100

    start to save a first set of scripts
    to generate the geometry and to run mfem/mgis

---
+++ scripts/25spheres/25spheres.py
@@ -0,0 +1,47 @@
+import sac_de_billes as sdb
+import merope
+
+dimensions_side:int = 5  # µm
+
+L = [dimensions_side, dimensions_side, dimensions_side]
+
+radius = 0.5  # µm
+pore_fraction = 0.10  # /
+distMin = 0.150
+randomSeed = 1
+
+
+theSpheres = sdb.throwSpheres_3D(sdb.TypeAlgo.RSA, sdb.NameShape.Tore, L, randomSeed, [
+    [radius, pore_fraction]], [2], distMin)
+
+
+tab_phase_not_to_mesh = []
+for j in range(0, len(theSpheres)):
+    sphere = theSpheres[j]
+    sphere.phase = 2+j
+    tab_phase_not_to_mesh.append(sphere.phase)
+
+with open("bubbles.txt", 'w') as bbl:
+    for sphere in theSpheres:
+        bbl.write(str(sphere.phase) + " ")
+        bbl.write(str(sphere.center[0]) + " " +
+                  str(sphere.center[1]) + " " +
+                  str(sphere.center[2]) + " ")
+        bbl.write(str(sphere.radius) + "\n")
+
+sphInc = merope.SphereInclusions_3D()
+sphInc.setLength(L)
+sphInc.setSpheres(theSpheres)
+
+mi = merope.MultiInclusions_3D()
+mi.setInclusions(sphInc)
+mi.setMatrixPhase(1)
+
+meshGenerator = merope.mesh.MeshGenerator()
+meshGenerator.setMeshOrder(2)
+meshGenerator.setMeshSize(0.3)
+meshGenerator.setMultiInclusions(mi)
+meshGenerator.do_not_mesh(tab_phase_not_to_mesh)
+test_name=str(dimensions_side)+"um_"+str(pore_fraction*100).split(".")[0]+"_pct"
+meshGenerator.set_nameOutput([test_name+".vtk"])
+meshGenerator.write("mesh_"+test_name+".geo")
---
+++ scripts/25spheres/run_case.sh
@@ -0,0 +1,5 @@
+
+compil_dir=/TMP/tb263902/projects/20251222/mm-opera-hpc/build/bubble/src
+
+mpirun -n 6 ./test-bubble --dmin 0.6 --pref 1e8 -v 1 -l $compil_dir/libBehaviour.so -f ./bubbles.txt -m ./mesh_5um_10_pct.msh -sf 0.2
+#./test-bubble --dmin 0.6 --pref 1e8 -v 1 -l $compil_dir/libBehaviour.so -f ./bubbles.txt -m ./mesh_5um_10_pct.msh -sf 0.2
