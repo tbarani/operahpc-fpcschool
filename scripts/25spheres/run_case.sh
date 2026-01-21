7ccc66217d343218ce10e03c8d97d57678075092
Tommaso Barani <tommaso.barani@cea.fr>
2026-01-21 10:48:30 +0100

    Update run_case.sh

--- scripts/25spheres/run_case.sh
+++ scripts/25spheres/run_case.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-compil_dir=/TMP/tb263902/projects/20251222/mm-opera-hpc/build/bubble/src
+compil_dir=$MMM_LIB_DIR
 
 mpirun -n 6 ./test-bubble --dmin 0.6 --pref 1e8 -v 1 -l $compil_dir/libBehaviour.so -f ./bubbles.txt -m ./mesh_5um_10_pct.msh -sf 0.2
 #./test-bubble --dmin 0.6 --pref 1e8 -v 1 -l $compil_dir/libBehaviour.so -f ./bubbles.txt -m ./mesh_5um_10_pct.msh -sf 0.2
